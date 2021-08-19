b36f326abf0abcc4bc215f5583414c0d3accb832
Ben Steger <69767810+ben-steger@users.noreply.github.com>
2021-08-19 16:39:33 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,5 @@ export default function Index() {
   //const projectDir = process.cwd()
 //   const projUser = process.env.USER;
   let os = require('os')
-  return <h1>{os.userInfo()}</h1>;
+  return <h1>{os.userInfo().username}</h1>;
 }
