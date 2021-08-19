24358a3b54e35831bb84742a9187e127e9fd9485
Ben Steger <69767810+ben-steger@users.noreply.github.com>
2021-08-19 11:27:46 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
 export default function Index() {
+  const { exec } = require("child_process");
+  exec("ls -la", (error, stdout, stderr) => {
+      if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+      }
+      if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          return;
+      }
+      console.log(`stdout: ${stdout}`);
+  });
   return <h1>BOOP</h1>;
 }
